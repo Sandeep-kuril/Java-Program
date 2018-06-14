425c30c978e02c54c2f15a3e2413927e31b0e3e8
Sandeep <30601506+Sandeep-kuril@users.noreply.github.com>
2018-06-14 21:59:00 +0530

    First Commit

---
+++ eclipse-workspace/Binary/src/Biny.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Biny {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       Scanner sc=new Scanner(System.in);
+       System.out.println("Enter any number to convert into Binary");
+       int num=sc.nextInt();
+       System.out.println("Binary Number is:"+Integer.toBinaryString(num)); 
+	}
+
+}
